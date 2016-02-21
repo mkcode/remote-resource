d33b3969ec2baf4c60d284a4de226180b02bb03a
Chris Ewald <chrisewald@gmail.com>
2016-02-21 18:20:11 -0500

    add dsl for defining API related on Base

---
+++ lib/api_cached_attributes/dsl.rb
@@ -0,0 +1,28 @@
+module ApiCachedAttributes
+  # Our humble DSL
+  module DSL
+    attr_reader :client_proc, :responses, :cached_attributes
+
+    def client(&block)
+      if block
+        @client_proc = block
+      end
+    end
+
+    def named_response(name, &block)
+      if block
+        @responses ||= {}
+        @responses[name] = block
+      end
+    end
+
+    def default_response(&block)
+      named_response(:default, block)
+    end
+
+    def api_cached_attr(method, named_response = :default)
+      @cached_attributes ||= {}
+      @cached_attributes[method] = {}
+    end
+  end
+end
