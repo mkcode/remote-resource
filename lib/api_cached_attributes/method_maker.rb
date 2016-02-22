bc2d896815c49130a62ccd593490cda16bb1b929
Chris Ewald <chrisewald@gmail.com>
2016-02-22 13:24:40 -0500

    update method maker

--- lib/api_cached_attributes/method_maker.rb
+++ lib/api_cached_attributes/method_maker.rb
@@ -1,24 +1,37 @@
+require_relative './lookup_service'
+
 module ApiCachedAttributes
   class ApiReadOnlyMethod < StandardError; end;
 
   # Our humble method maker
   class MethodMaker
-    def initialize(attributes_class, options)
-      @attributes_class = attributes_class
+    def initialize(base_class, options)
+      @base_class = base_class
       @options = options
     end
 
-    def define_methods_on(klass_object)
-      @attributes_class.cached_attributes.each_pair do |method, value|
-        klass_object.class_eval <<-RUBY, __FILE__, __LINE__ + 1
+    def define_methods_on(target_class)
+      lookup_service = LookupService.new(@base_class, @options)
+      lookup_target_var = "@#{@base_class.underscore}_lookup".to_sym
+      target_class.instance_variable_set(lookup_target_var, lookup_service)
+
+      @base_class.cached_attributes.each_pair do |method, value|
+        target_class.class_eval <<-RUBY, __FILE__, __LINE__ + 1
           def #{method}
-            puts "woot"
+            key_peices = {
+              method: "#{method}",
+              attributes_class: "#{@base_class.name}",
+              # response_name: #{},
+              scope: #{@options[:scope]}
+            }
+            key = CacheKey.for(key_peices)
+            # CachedResponseLookup.find()
           end
 
           def #{method}=(other)
             msg = "`#{method}` was created by the `ApiCachedAttributes` gem, which "
             msg += "only supports allows for getters from the API. Though, you "
-            msg += "may override this method on `#{klass_object.name}`."
+            msg += "may override this method on `#{target_class.name}`."
             fail ApiCachedAttributes::ApiReadOnlyMethod.new(msg)
           end
         RUBY
