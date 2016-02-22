578d28e934c1a6e646b2769bfa5a4f26d0377dc7
Chris Ewald <chrisewald@gmail.com>
2016-02-22 16:48:36 -0500

    scope is always an array

--- lib/api_cached_attributes/bridge.rb
+++ lib/api_cached_attributes/bridge.rb
@@ -4,6 +4,7 @@ module ApiCachedAttributes
   # Our humble bridge
   module Bridge
     def api_cached_attributes(which_klass, options = {})
+      options[:scope] = Array(options[:scope])
       klass = ApiCachedAttributes.get_attributes_class(which_klass)
       maker = LookupServiceAttacher.new(klass, options)
       maker.define_methods_on(self)
