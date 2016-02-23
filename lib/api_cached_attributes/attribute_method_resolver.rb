86835bbbd8a7dd8debce7feabe4d1ce97079f299
Chris Ewald <chrisewald@gmail.com>
2016-02-23 16:07:22 -0500

    use cache client and cache control in lookup

--- lib/api_cached_attributes/attribute_method_resolver.rb
+++ lib/api_cached_attributes/attribute_method_resolver.rb
@@ -1,5 +1,6 @@
 require_relative './evaluator'
-require_relative './db_cache'
+require_relative './cache_client'
+require_relative './cache_control'
 
 module ApiCachedAttributes
   # Our humble lookup service
@@ -16,14 +17,24 @@ module ApiCachedAttributes
 
     def get(method, scope, named_resource = :default, target_instance)
       key = key_for(method, scope, named_resource)
-      @db_cache.target_instance = target_instance
-      value = @db_cache.read_key(key)
-      if value
-        puts 'DB HIT'
-        return value
-      end
-      puts 'DB MISS'
+
       @evaluator.client_scope = scope
+      @db_cache.target_instance = target_instance
+
+      cache_client = CacheClient.new(@evaluator.client)
+      headers = cache_client.headers(@base_class.resources[:default])
+      cache_control = CacheControl.new(headers['cache-control'])
+
+      unless false # cache_control.private?
+        # return redis_value if @REDIS.read_key(key)
+        db_value = @db_cache.read_key(key)
+        if db_value
+          puts 'DB HIT'
+          return db_value
+        end
+        puts 'DB MISS'
+      end
+
       resource = @evaluator.resource(named_resource)
       value = resource.send(method.to_sym)
       @db_cache.write_key(key, value)
