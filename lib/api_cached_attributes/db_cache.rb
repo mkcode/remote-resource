25955e8417b1243d702266c4e0471c248dd4a328
Chris Ewald <chrisewald@gmail.com>
2016-02-23 09:04:05 -0500

    complete db_cache

--- lib/api_cached_attributes/db_cache.rb
+++ lib/api_cached_attributes/db_cache.rb
@@ -1,3 +1,5 @@
+require_relative '../serializers/marshal'
+
 module ApiCachedAttributes
   class DBCache
     ADAPTERS = %i(active_record)
@@ -5,19 +7,30 @@ module ApiCachedAttributes
     attr_reader :column_name
     attr_accessor :target_instance
 
-    def initialize(adapter, column_name)
+    def initialize(adapter, column_name, serializer = :marshal)
       @adapter = :active_record
-      @column_name = column_name
+      @column_name = column_name.to_sym
+      @serializer = Serializers::MarshalSerializer.new
     end
 
-    def db_cache_value
-      @target_instance.send(@column_name.to_sym)
+    # always returns a hash
+    def read_column
+      raw = @target_instance.read_attribute(column_name)
+      raw ? @serializer.load(raw) : {}
     end
 
-    def read(key)
+    def write_column(hash)
+      fail ArgumentError 'must be a hash!' unless hash.is_a? Hash
+      raw = @serializer.dump(hash)
+      @target_instance.update_attribute(column_name, raw)
     end
 
-    def write(key, value)
+    def read_key(key)
+      read_column[key.to_sym]
+    end
+
+    def write_key(key, value)
+      write_column(read_column.merge(key.to_sym => value))
     end
   end
 end
