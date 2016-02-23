74c64037cf597ee2eec4d03a6c64e25c2f8766ee
Chris Ewald <chrisewald@gmail.com>
2016-02-22 22:41:05 -0500

    document bridge. entry point

--- lib/api_cached_attributes/bridge.rb
+++ lib/api_cached_attributes/bridge.rb
@@ -1,8 +1,47 @@
 require_relative './lookup_service_attacher'
+require_relative './db_cache_attacher'
 
 module ApiCachedAttributes
   # Our humble bridge
   module Bridge
+    # Setup the api cached attributes on this class.
+    #
+    # Behind the scenes, this is copying / and creating cache and api getter
+    # methods to the class. Copies everything that is needed to begin using
+    # these features.
+    #
+    # arguments:
+    #
+    #   - api_name: the api configuration class to include on this object.
+    #               api_name is an underscored symbol type that has the last
+    #               `Attributes` suffix dropped. Example:
+    #                 (class) GithubUserAttributes => (symbol) :github_user
+    #
+    #   - options:  A hash of options. Keys are always symbols.
+    #
+    #     - scope:  methods set on this class for which the api is unique. May
+    #               be a single symbol value or an array of symbols.
+    #
+    #     - cache_column: name of the column on this table to which the api
+    #                     should persist its cache. Only required when the
+    #                     default (magic) column name is not wanted.
+    #
+    # example:
+    #
+    #   class Repo < ActiveRecord::Base
+    #     api_cached_attributes :github_repo, scope: [:login, :user]
+    #
+    #     ...
+    #   end
+    #
+    #   The above `api_cache_attributes` call assumes that the following class
+    #   exists:
+    #
+    #   class GithubRepoAttributes < ApiCachedAttributes::Base
+    #     ...
+    #   end
+    #
+    # returns nil
     def api_cached_attributes(which_klass, options = {})
       ensure_valid_options!(options)
       # Fetch the configuration base class for this api
