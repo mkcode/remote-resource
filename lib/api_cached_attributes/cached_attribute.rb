8fc81f8e9dd9e52ff05db1b73fc59be38aac75c7
Chris Ewald <chrisewald@gmail.com>
2016-02-24 16:07:20 -0500

    finds resource_name from base_class. client scope handling

--- lib/api_cached_attributes/cached_attribute.rb
+++ lib/api_cached_attributes/cached_attribute.rb
@@ -1,23 +1,37 @@
 module ApiCachedAttributes
+  class ScopeNotSet < StandardError; end
+
   # Yes
   class CachedAttribute
-    attr_reader :name, :resource_name
+    attr_reader :name, :base_class
+    attr_accessor :client_scope
 
     def initialize(name, base_class)
       @name = name
       @base_class = base_class
+      @client_scope = false
     end
     alias_method :method, :name
 
+    def resource_name
+      @base_class.cached_attributes[@name]
+    end
+
+    # nil is a possible valid value for @client_scope when there is no scope
+    def client_scope?
+      @client_scope != false
+    end
+
     def client
+      fail ScopeNotSet if @client_scope == false
       @base_class.client_proc.call(client_scope)
     end
 
-    def resource(name)
-      if resource = @base_class.resources[name]
-        resource.call(client)
+    def resource(override_client = nil)
+      if resource = @base_class.resources[resource_name]
+        resource.call(override_client || client)
       else
-        fail ArgumentError.new("there is no resource named #{name} on #{name}.")
+        fail ArgumentError.new("there is no resource named #{name} on #{@name}.")
       end
     end
   end
