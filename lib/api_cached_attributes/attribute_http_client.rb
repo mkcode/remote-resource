f056e7ac623088ab6e58073ee085bb64cc02ca44
Chris Ewald <chrisewald@gmail.com>
2016-02-24 16:12:42 -0500

    new AttributeHttpClient class

---
+++ lib/api_cached_attributes/attribute_http_client.rb
@@ -0,0 +1,24 @@
+module ApiCachedAttributes
+  class AttributeHttpClient
+    def initialize(attribute, client = nil)
+      @attribute = attribute
+      @client = client || @attribute.client
+      @resource_name = @attribute.resource_name
+    end
+
+    def headers_only
+      with_head_only_request do |client|
+        @attribute.resource(client)
+      end
+      @client.last_response.headers
+    end
+
+    def with_head_only_request
+      client_class = @client.singleton_class
+      client_class.send(:alias_method, :orig_get, :get)
+      client_class.send(:alias_method, :get, :head)
+      yield @client
+      client_class.send(:alias_method, :get, :orig_get)
+    end
+  end
+end
