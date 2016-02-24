956a4d627b453924fd071948d065e767f489059b
Chris Ewald <chrisewald@gmail.com>
2016-02-24 14:33:50 -0500

    reworking resolver to use CachedAttributes

--- lib/api_cached_attributes/attribute_method_resolver.rb
+++ lib/api_cached_attributes/attribute_method_resolver.rb
@@ -1,11 +1,12 @@
 require_relative './evaluator'
 require_relative './cache_client'
 require_relative './cache_control'
+require_relative './cached_attribute'
 
 module ApiCachedAttributes
   # Our humble lookup service
   class AttributeMethodResolver
-    attr_reader :key_prefix
+    attr_reader :key_prefix, :attributes
     attr_accessor :evaluator, :db_cache
 
     def initialize(base_class, options)
@@ -13,17 +14,45 @@ module ApiCachedAttributes
       @options = options
       @evaluator = nil
       @db_cache = nil
+      @attributes = create_cached_attributes!
+    end
+
+    def attribute(name)
+      @attributes.detect { |attr| attr.name == name }
+    end
+
+    def create_cached_attributes!
+      @base_class.cached_attributes.map do |method, value|
+        CachedAttribute.new(method, value)
+      end
     end
 
     def get(method, scope, named_resource = :default, target_instance)
       key = key_for(method, scope, named_resource)
 
-      @evaluator.client_scope = scope
-      @db_cache.target_instance = target_instance
+      attribute(method)
+      binding.pry
 
-      cache_client = CacheClient.new(@evaluator.client)
-      headers = cache_client.headers(@base_class.resources[:default])
-      cache_control = CacheControl.new(headers['cache-control'])
+      # moc = MethodOverideClient.new( client )
+      # response_headers = moc.headers_only( resources[:default] )
+      # cache_resolver = ResponseCache.new( response_headers )
+
+
+
+      # @evaluator.client_scope = scope
+      # @db_cache.target_instance = target_instance
+
+      # cache_client = CacheClient.new(@evaluator.client)
+      # headers = cache_client.headers(@base_class.resources[:default])
+      # cache_control = CacheControl.new(headers['cache-control'])
+
+      # moc = MethodOverideClient.new( client )
+      # response_headers = moc.headers_only( resources[:default] )
+      # cache_resolver = ResponseCache.new( response_headers )
+      #   => resources[:default]
+
+
+      # AttributeLookupService
 
       unless false # cache_control.private?
         # return redis_value if @REDIS.read_key(key)
