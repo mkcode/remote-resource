310769ef2b6e9b0a13d0ec9106511c3437b0ed0b
Chris Ewald <chrisewald@gmail.com>
2016-02-26 16:34:18 -0500

    storages is an array

--- lib/api_cached_attributes/storage.rb
+++ lib/api_cached_attributes/storage.rb
@@ -2,7 +2,7 @@ module ApiCachedAttributes
   # Our humble storage
   module StorageRegistration
     def self.extended(klass)
-      klass.instance_variable_set(:@storages, {})
+      klass.instance_variable_set(:@storages, [])
     end
 
     def storages=(storages)
