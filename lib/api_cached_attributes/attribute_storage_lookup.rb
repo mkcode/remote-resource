2c354b4422b88cbef71d940a81c079dccabec586
Chris Ewald <chrisewald@gmail.com>
2016-02-26 16:35:18 -0500

    add storage lookup

---
+++ lib/api_cached_attributes/attribute_storage_lookup.rb
@@ -0,0 +1,37 @@
+module ApiCachedAttributes
+  class AttributeStorageLookup
+    def initialize(attribute)
+      @attribute = attribute
+    end
+
+    def headers
+      storage_entry.headers
+    end
+
+    def data
+      storage_entry.data
+    end
+
+    def storages
+      ApiCachedAttributes.storages
+    end
+
+    def write(storage_entry)
+      storages.each do |storage|
+        storage.write_key(@attribute.key.for_storage, storage_entry)
+      end
+    end
+
+    private
+
+    def storage_entry
+      return @storage_entry if @storage_entry
+      storages.each do |storage|
+        if storage_entry = storage.read_key(@attribute.key.for_storage)
+          @storage_entry = storage_entry
+          return @storage_entry
+        end
+      end
+    end
+  end
+end
