c065262b38222af19fbcdaadacade4c87de3bd7d
Chris Ewald <chrisewald@gmail.com>
2016-02-28 19:37:51 -0500

    partial function head with headers in AttributeHttpClient

--- lib/api_cached_attributes/attribute_http_client.rb
+++ lib/api_cached_attributes/attribute_http_client.rb
@@ -6,8 +6,8 @@ module ApiCachedAttributes
       @resource_name = @attribute.resource_name
     end
 
-    def headers_only
-      with_head_only_request do |client|
+    def headers_only(additional_headers = {})
+      with_head_only_request(additional_headers) do |client|
         @attribute.resource(client)
       end
       @client.last_response.headers
@@ -18,12 +18,21 @@ module ApiCachedAttributes
       @client.last_response
     end
 
-    def with_head_only_request
+    def with_head_only_request(headers = {})
+      head_method_partial_with_headers!(headers) if headers && headers.size > 0
       client_class = @client.singleton_class
       client_class.send(:alias_method, :orig_get, :get)
       client_class.send(:alias_method, :get, :head)
       yield @client
       client_class.send(:alias_method, :get, :orig_get)
+    end
+
+    def head_method_partial_with_headers!(headers)
+      client_class = @client.singleton_class
+      client_class.send(:alias_method, :orig_head, :head)
+      client_class.send(:define_method, :head) do |url, _|
+        orig_head(url, headers: headers)
+      end
     end
   end
 end
