d1785c177ee8afbba783ad47395317ad4a220c26
Chris Ewald <chrisewald@gmail.com>
2016-03-02 18:24:06 -0500

    rename storage to storage_registration

--- lib/api_cached_attributes.rb
+++ lib/api_cached_attributes.rb
@@ -2,7 +2,7 @@ require 'api_cached_attributes/version'
 require 'api_cached_attributes/dsl'
 require 'api_cached_attributes/bridge'
 require 'api_cached_attributes/subclass_registration'
-require 'api_cached_attributes/storage'
+require 'api_cached_attributes/storage_registration'
 require 'active_support/concern'
 require 'active_support/core_ext/string'
 
