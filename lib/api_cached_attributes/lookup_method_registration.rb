ea2613dc2b890f8e6cc4c05439455ab58c45e82a
Chris Ewald <chrisewald@gmail.com>
2016-03-02 18:49:53 -0500

    set application default lookup method

--- lib/api_cached_attributes/lookup_method_registration.rb
+++ lib/api_cached_attributes/lookup_method_registration.rb
@@ -1,8 +1,10 @@
+require_relative 'attribute_lookup'
+
 module ApiCachedAttributes
   # Our humble storage
   module LookupMethodRegistration
     def self.extended(klass)
-      klass.instance_variable_set(:@lookup_method, [])
+      klass.instance_variable_set(:@lookup_method, nil)
     end
 
     def lookup_method=(lookup_method)
@@ -10,7 +12,11 @@ module ApiCachedAttributes
     end
 
     def lookup_method
-      @lookup_method
+      @lookup_method || default_lookup_method
+    end
+
+    def default_lookup_method
+      AttributeLookup.new
     end
   end
 end
