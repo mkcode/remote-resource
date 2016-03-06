94515747189e4afaf0003f28b1e7d0bf830c7228
Chris Ewald <chrisewald@gmail.com>
2016-03-06 15:30:55 -0500

    basic spec support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'api_cached_attributes'
+
+require 'pry'
+
+Dir.glob('./spec/support/**/*.rb').each { |f| require f }
---
+++ spec/support/class_helpers.rb
@@ -0,0 +1,10 @@
+
+
+def stub_class name, superclass = nil, &block
+  stub_const(name.to_s.camelize, Class.new(superclass || Object, &block))
+end
+
+def base_class(name, &block)
+  class_name = name.end_with?('Attributes') ? name : name + 'Attributes'
+  stub_class(class_name, ApiCachedAttributes::Base, &block)
+end
