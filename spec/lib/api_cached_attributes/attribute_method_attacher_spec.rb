3aa3a7556153517c7c44fb78a1e570cf4270318a
Chris Ewald <chrisewald@gmail.com>
2016-03-09 16:11:45 -0500

    fixup Attacher spec

--- spec/lib/api_cached_attributes/attribute_method_attacher_spec.rb
+++ spec/lib/api_cached_attributes/attribute_method_attacher_spec.rb
@@ -1,21 +1,22 @@
 require 'spec_helper'
 
 describe ApiCachedAttributes::AttributeMethodAttacher do
-  let(:attributes_class) do
-    base_class 'GithubUser' do
-      attribute(:user)
-    end
-  end
+  let(:attributes_class) { base_class 'GithubUser' }
+  subject                { described_class.new(attributes_class) }
+  let(:target_class)     { Class.new }
 
   describe '.attach_to' do
-    subject { described_class.new(attributes_class, test_key: 'value') }
-    let(:target_class) { Class.new }
+    before { subject.attach_to(target_class) }
 
     it 'sets an AttributeMethodResolver on the supplied class' do
-      subject.attach_to(target_class)
       expected_var_name = subject.send(:method_resolver_var)
       expect(target_class.instance_variable_get(expected_var_name))
         .to be_an ApiCachedAttributes::AttributeMethodResolver
+    end
+
+    it 'includes the AttributeMethods on the supplied class' do
+      expect(target_class.included_modules.map(&:class))
+        .to include ApiCachedAttributes::AttributeMethods
     end
   end
 end
