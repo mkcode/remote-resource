48ee6dac7b6c2f1dbd0b7a510b2fae1ba1d917e7
Chris Ewald <chrisewald@gmail.com>
2016-03-11 02:02:28 -0500

    add coverage to spec_helper & rake task to invoke

--- Rakefile
+++ Rakefile
@@ -4,3 +4,9 @@ require 'rspec/core/rake_task'
 RSpec::Core::RakeTask.new(:spec)
 
 task default: :spec
+
+desc "Run RSpec with code coverage"
+task :coverage do
+  ENV['COVERAGE'] = 'true'
+  Rake::Task["spec"].execute
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
+if ENV['COVERAGE']
+  require 'codeclimate-test-reporter'
+  SimpleCov.start do
+    add_filter '/spec/'
+    add_group 'Core', ['lib', 'lib/api_cached_attributes']
+    add_group 'Storage', 'lib/api_cached_attributes/storage'
+    add_group 'Lookup', 'lib/api_cached_attributes/lookup'
+  end
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'api_cached_attributes'
 
