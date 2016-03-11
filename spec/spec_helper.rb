b45f32a20b50012b8d00b9e96b0355b58b2cb884
Chris Ewald <chrisewald@gmail.com>
2016-03-11 02:10:27 -0500

    covering correct groups and files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,17 @@ if ENV['COVERAGE']
   require 'codeclimate-test-reporter'
   SimpleCov.start do
     add_filter '/spec/'
-    add_group 'Core', ['lib', 'lib/api_cached_attributes']
-    add_group 'Storage', 'lib/api_cached_attributes/storage'
-    add_group 'Lookup', 'lib/api_cached_attributes/lookup'
+    add_group 'Core', [*Dir.glob('lib/*.rb'),
+                       *Dir.glob('lib/api_cached_attributes/*.rb'),
+                       'lib/api_cached_attributes/configuration']
+    add_group 'Storage', 'lib/api_cached_attributes/storage/*'
+    add_group 'Lookup', 'lib/api_cached_attributes/lookup/*'
   end
 end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'api_cached_attributes'
+require 'api_cached_attributes/storage/redis'
 
 require 'pry'
 
