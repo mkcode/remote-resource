6fecac101ecebb84347ce25fa4c3a4590898409b
Chris Ewald <chrisewald@gmail.com>
2016-03-10 20:33:18 -0500

    add fake_octokit_client helper

--- spec/support/class_helpers.rb
+++ spec/support/class_helpers.rb
@@ -9,3 +9,10 @@ def base_class(name, &block)
   class_name = name.end_with?('Attributes') ? name : name + 'Attributes'
   stub_class(class_name, ApiCachedAttributes::Base, &block)
 end
+
+def fake_octokit_client
+  fake = double()
+  user_response = double()
+  allow(user_response).to receive(:login).and_return('mkcode')
+  allow(fake).to receive(:user).and_return(user_response)
+end
