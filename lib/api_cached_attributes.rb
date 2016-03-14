3bcc29ed710a1a76ce9d4ed1a04916c304d01bf2
Chris Ewald <chrisewald@gmail.com>
2016-03-14 17:00:18 -0400

    ensure that the eager_load class is loaded

--- lib/api_cached_attributes.rb
+++ lib/api_cached_attributes.rb
@@ -24,10 +24,16 @@ module ApiCachedAttributes
 
     class << self
       def find_descendant(which_class)
+        ensure_loaded(which_class)
         descendants.detect do |descendant|
           [descendant.underscore.to_sym, descendant.short_sym]
             .include? which_class.to_sym
         end
+      end
+
+      def ensure_loaded(which_class)
+        which_class.to_s.camelize.concat('Attributes').safe_constantize ||
+          which_class.to_s.camelize.safe_constantize
       end
 
       def underscore
