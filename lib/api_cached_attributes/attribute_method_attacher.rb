d4bc4e30c80bcc80e44df8d4a8f0620d0ec1c48a
Chris Ewald <chrisewald@gmail.com>
2016-03-14 23:24:40 -0400

    improve MethodAttacher
    add documentation to all methods on attacher. Add a prefix and
    attributes_map options which override defined method names

--- lib/api_cached_attributes/attribute_method_attacher.rb
+++ lib/api_cached_attributes/attribute_method_attacher.rb
@@ -1,18 +1,47 @@
 require 'api_cached_attributes/attribute_method_resolver'
 
 module ApiCachedAttributes
-  # non-anonymous namespace for our generated methods.
+  # non-anonymous namespace for our generated methods. Mixed into the target
+  # class so that introspection shows where the methods came from.
   class AttributeMethods < Module; end
 
-  # Our humble lookup service attacher
+  # Public: Creates an instance of the AttributeMethodAttacher, which is
+  # responsible for setting up and attaching methods onto a target class which
+  # are used to lookup cached attributes.
   class AttributeMethodAttacher
+    attr_accessor :options
+    # Public: Creates an instance of the AttributeMethodAttacher, which is
+    # responsible for setting up and attaching methods onto a target class which
+    # are used to lookup cached attributes.
+    #
+    # base_class - the class which will have methods attached to.
+    # options - options hash with the following keys (default: {}). Generally,
+    #           prefix should be used when overriding the names of all the
+    #           methods is desired and attributes_map should be used when
+    #           overriding only a few method names is desired.
+    #     :prefix - prefix for the names of the newly created methods.
+    #     :attributes_map - a hash for overriding method names to create. The
+    #                       keys in this hash represent an attribute defined on
+    #                       the base_class. The values are the overriding name
+    #                       of the method to be defined on the target_class.
+    #
+    # Returns a new instance.
     def initialize(base_class, options = {})
       @base_class = base_class
-      @options = options
+      @options = ensure_options(options)
     end
 
+    # Public: Set the base_classes' attribute methods on the given target class.
+    # Sets a MethodResolver instance on the target_class as well. Logs warnings
+    # if any methods on the target class are overwritten.
+    #
+    # target_class - The class upon which the base_classes' attribute methods
+    #                should be set.
+    #
+    # Returns the target_class
     def attach_to(target_class)
       method_resolver = AttributeMethodResolver.new(@base_class, @options)
+      overwrite_method_warnings(target_class)
 
       target_class.instance_variable_set(method_resolver_var, method_resolver)
       target_class.send(:include, make_attribute_methods_module)
@@ -20,26 +49,74 @@ module ApiCachedAttributes
 
     private
 
+    # Internal: Returns a module with the base_classes' attributes defined as
+    # getter and setter methods.
     def make_attribute_methods_module
       attribute_methods_module = AttributeMethods.new
 
-      @base_class.attributes.keys.each do |method|
+      @base_class.attributes.keys.each do |attributes_method|
+        method_name = @options[:attributes_map][attributes_method]
+        method_name = "#{@options[:prefix]}_#{method_name}" if @options[:prefix]
         attribute_methods_module.module_eval <<-RUBY, __FILE__, __LINE__ + 1
-          def #{method}
+          def #{method_name}
             self.class.instance_variable_get(:#{method_resolver_var})
-                      .get(:#{method}, self)
+                      .get(:#{attributes_method}, self)
           end
 
-          def #{method}=(other)
-            fail ApiReadOnlyMethod.new('#{method}')
+          def #{method_name}=(other)
+            fail ApiReadOnlyMethod.new('#{method_name}')
           end
         RUBY
       end
       attribute_methods_module
     end
 
+    # Internal: The name of the variable on the target class which holds the
+    # instance of the ApiCachedAttributes::MethodResolver.
     def method_resolver_var
       "@#{@base_class.underscore}_resolver".to_sym
+    end
+
+    # Internal: Populate the attributes map. If no attributes_map arg is given,
+    # then attributes_map is a 1 to 1 map (the default_attributes_map below).
+    def ensure_options(options)
+      options[:attributes_map] = default_attributes_map
+                                 .merge(options[:attributes_map] || {})
+      options
+    end
+
+    # Internal: Create a basic 1 to 1 (key to value) Hash map of the attribute
+    # names. This is intended to be overridden by the attributes_map argument.
+    def default_attributes_map
+      {}.tap do |attr_map|
+        @base_class.attributes.keys.each do |method|
+          attr_map[method.to_sym] = method.to_sym
+        end
+      end
+    end
+
+    # Internal: Log warning methods when this attacher will overwrite methods on
+    # the target class.
+    def overwrite_method_warnings(target_class)
+      @options[:attributes_map].values.each do |method|
+        method = "#{@options[:prefix]}_#{method}" if @options[:prefix]
+        if present_and_future_public_methods(target_class).include? method
+          log_msg =  "#{@base_class.name} is overwriting the "
+          log_msg += "#{method} method on #{target_class.name}"
+          ApiCachedAttributes.logger.warn log_msg
+        end
+      end
+    end
+
+    # Internal: ActiveRecord objects do not define a model's column methods
+    # until the instance is created. In this context, we have the class, so we
+    # lookup the 'future methods' on the `column_names` class method.
+    def present_and_future_public_methods(target_class)
+      conflicts = target_class.public_instance_methods
+      if target_class.respond_to? :column_names
+        conflicts += target_class.column_names.map(&:to_sym)
+      end
+      conflicts
     end
   end
 end
--- spec/lib/api_cached_attributes/attribute_method_attacher_spec.rb
+++ spec/lib/api_cached_attributes/attribute_method_attacher_spec.rb
@@ -10,22 +10,46 @@ describe ApiCachedAttributes::AttributeMethodAttacher do
     Class.new.tap { |c| c.send(:include, ObjectIntrospectionHelpers) }
   end
   subject { described_class.new(attributes_class) }
-  before  { subject.attach_to(target_class) }
 
   describe '.attach_to' do
-    it 'sets an AttributeMethodResolver on the supplied class' do
-      expected_var_name = subject.send(:method_resolver_var)
-      expect(target_class.ivar_get(expected_var_name))
-        .to be_an ApiCachedAttributes::AttributeMethodResolver
+    context 'on the supplied (target) class' do
+      before  { subject.attach_to(target_class) }
+
+      it 'sets an AttributeMethodResolver on the supplied class' do
+        expected_var_name = subject.send(:method_resolver_var)
+        expect(target_class.ivar_get(expected_var_name))
+          .to be_an ApiCachedAttributes::AttributeMethodResolver
+      end
+
+      it 'includes the AttributeMethods on the supplied class' do
+        expect(target_class.included_modules.map(&:class))
+          .to include ApiCachedAttributes::AttributeMethods
+      end
     end
 
-    it 'includes the AttributeMethods on the supplied class' do
-      expect(target_class.included_modules.map(&:class))
-        .to include ApiCachedAttributes::AttributeMethods
+    context 'when overwriting a method on the target class' do
+      it 'logs a warning message' do
+        expect(ApiCachedAttributes.logger).to receive(:warn)
+        target_class = Class.new
+        target_class.send(:define_method, :login, -> {})
+        subject.attach_to(target_class)
+      end
+    end
+
+    context 'when the prefix option distinguishes the methods' do
+      it 'does not log a warning message' do
+        subject.options.merge!({ prefix: 'prefixed' })
+        expect(ApiCachedAttributes.logger).to_not receive(:warn)
+        target_class = Class.new
+        target_class.send(:define_method, :login, -> {})
+        subject.attach_to(target_class)
+      end
     end
   end
 
   describe 'attacher defined methods' do
+    before  { subject.attach_to(target_class) }
+
     it 'calling a getter method calls get on the method resolver' do
       method = attributes_class.attributes.keys.first
       expected_var_name = subject.send(:method_resolver_var)
@@ -65,6 +89,23 @@ describe ApiCachedAttributes::AttributeMethods do
     attributes_class.attributes.each_pair do |attr, _|
       setter_name = (attr.to_s + '=').to_sym
       expect(subject.public_instance_methods).to include(setter_name)
+    end
+  end
+
+  it 'respects the constructors prefix argument' do
+    attacher_class.options.merge!(prefix: 'prefixed')
+    attributes_class.attributes.each_pair do |attr, _|
+      prefixed_name = ('prefixed_' + attr.to_s).to_sym
+      expect(subject.public_instance_methods).to include(prefixed_name)
+    end
+  end
+
+  it 'respects the constructors attributes_map argument' do
+    attr_map = attacher_class.options[:attributes_map].merge(login: :overide)
+    attacher_class.options.merge!(attributes_map: attr_map)
+    attributes_class.attributes.each_pair do |attr, _|
+      attr_with_overides = attr.to_s.sub('login', 'overide').to_sym
+      expect(subject.public_instance_methods).to include(attr_with_overides)
     end
   end
 end
