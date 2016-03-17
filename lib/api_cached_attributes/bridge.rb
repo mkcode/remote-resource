063535e65407b253cbe31b5c10077bb146a0578a
Chris Ewald <chrisewald@gmail.com>
2016-03-16 21:12:48 -0400

    better document ApiCachedAttributes::Bridge

--- lib/api_cached_attributes/bridge.rb
+++ lib/api_cached_attributes/bridge.rb
@@ -1,34 +1,49 @@
 require 'api_cached_attributes/attribute_method_attacher'
 
 module ApiCachedAttributes
-  # Our humble bridge
+  # Extending a class with the ApiCachedAttributes::Bridge module will bring in
+  # the api_cached_atributes method, and therefore all of the functionality of
+  # the ApiCachedAttributes gem. In Rails, ActiveRecord is automatically
+  # extended with Bridge, and you would use the api_cached_attributes call like
+  # a familiar 'acts_as' method. Outside of Rails, or if used non ActiveRecord
+  # domain objects, extending your domain objects with Bridge is how to setup
+  # ApiCachedAttributes on those classes.
   module Bridge
-    # Setup the api cached attributes on this class.
+    # Public: Setup the provided api cached attributes on this class. This
+    # creates a getter (and setter) method on the calling class for each
+    # attribute defined in the referenced ApiCachedAttributes::Base class.
     #
-    # Behind the scenes, this is copying / and creating cache and api getter
-    # methods to the class. Copies everything that is needed to begin using
-    # these features.
+    # which_class - A symbol representing a descendant class of
+    #               ApiCachedAttributes::Base. The symbol is the underscored
+    #               version of the class name minus the 'Attributes' suffix.
+    #               Ex: (class) GithubUserAttributes => (symbol) :github_user
+    # options     - A hash of options. (default: {}) Always use symbols for
+    #               keys. These options are also passed into the MethodAttacher
+    #               and then to the MethodResolver.
+    #               :scope          - the scope option represents the context in
+    #                                 which this api resource is unique. It has
+    #                                 a similar meaning to ActiveRecord's
+    #                                 meaning of scope, as opposed the API
+    #                                 access meaning. The scope value can be a
+    #                                 Symbol, Array, or Hash. It is used to
+    #                                 build the scope argument, which is sent
+    #                                 into the client and resource blocks on the
+    #                                 Base attributes class. This argument to
+    #                                 these blocks is always a hash, whose
+    #                                 values were methods responses evaluated on
+    #                                 the target_class.
+    #               :prefix         - prefix for the names of the newly created
+    #                                 methods.
+    #               :attributes_map - A hash for overriding method names to
+    #                                 create. The keys in this hash represent an
+    #                                 attribute defined on the base_class. The
+    #                                 values are the overriding name of the
+    #                                 method to be defined on the target_class.
     #
-    # arguments:
-    #
-    #   - api_name: the api configuration class to include on this object.
-    #               api_name is an underscored symbol type that has the last
-    #               `Attributes` suffix dropped. Example:
-    #                 (class) GithubUserAttributes => (symbol) :github_user
-    #
-    #   - options:  A hash of options. Keys are always symbols.
-    #
-    #     - scope:  methods set on this class for which the api is unique. May
-    #               be a single symbol value or an array of symbols.
-    #
-    #     - cache_column: name of the column on this table to which the api
-    #                     should persist its cache. Only required when the
-    #                     default (magic) column name is not wanted.
-    #
-    # example:
+    # Examples
     #
     #   class Repo < ActiveRecord::Base
-    #     api_cached_attributes :github_repo, scope: [:login, :user]
+    #     api_cached_attributes :github_repo
     #
     #     ...
     #   end
@@ -39,6 +54,45 @@ module ApiCachedAttributes
     #   class GithubRepoAttributes < ApiCachedAttributes::Base
     #     ...
     #   end
+    #
+    #   scope option:
+    #
+    #   api_cached_attributes :github_user, scope: { id: :gh_user_id }
+    #
+    #   The scope hash in this example will evaluate the 'gh_user_id' method on
+    #   this class and send it in as the :id on the scope Hash. This style scope
+    #   argument is especially useful when the same attributes Base class is
+    #   mixed into multiple models, and therefore the multiple models are not
+    #   forced to use the same method names for the scope values. The scope hash
+    #   that is passed into the client and resource blocks will look like this:
+    #   { id: 2345987 }
+    #
+    #   api_cached_attributes :github_user, scope: [:id, :access_token]
+    #
+    #   This scope array will evaluate both the :id and :access_token methods on
+    #   this class. The scope hash that is passed into the client and resource
+    #   blocks will look like this: { id: 274346, access_token: '2ac4g3t7jf8'}
+    #
+    #   api_cached_attributes :github_user, scope: :github_login
+    #
+    #   This will evaluate the :github_login method on this class. The scope
+    #   hash that is passed into the client and resource blocks will look like
+    #   this: { github_login: 'mkcode' }
+    #
+    #   prefix and attribute_map options:
+    #
+    #   api_cached_attributes :github_user, prefix: :gh_user
+    #
+    #   All of the attribute methods defined on this class will be prefixed with
+    #   'gh_user_'. If the Base attributes class defined attributes :login and
+    #   :email, this class would have the methods: :gh_user_login and
+    #   :gh_user_email.
+    #
+    #   api_cached_attributes :github_user, attributes_map: { email: :gh_email }
+    #
+    #   The attributes_map argument here will override the name of the :email
+    #   attribute. Instead of the method being named :email, it will now be
+    #   named :gh_email.
     #
     # returns nil
     def api_cached_attributes(which_klass, options = {})
