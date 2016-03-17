2ed8868874d299baed4c5af95b6b9ec395fd7637
Chris Ewald <chrisewald@gmail.com>
2016-03-16 21:12:16 -0400

    document AttributesMethodResolver

--- lib/api_cached_attributes/attribute_method_resolver.rb
+++ lib/api_cached_attributes/attribute_method_resolver.rb
@@ -4,18 +4,56 @@ require 'api_cached_attributes/attribute_specification'
 require 'api_cached_attributes/notifications'
 
 module ApiCachedAttributes
-  # Our humble lookup service
+  # The AttributeMethodResolver class is responsible for for creating
+  # AttributeSpecifications with their scope and target object and kicking off
+  # the lookup process for the attribute. An instance of the
+  # AttributeMethodResolver class is created and saved on all domain objects
+  # that call api_cached_attributes. The generated attributes method on that
+  # domain class all call `get` on this class with the attribute method to
+  # actually lookup the attribute value. The options passed to constructor of
+  # this class are the same that are passed into the bridge.
   class AttributeMethodResolver
     include ApiCachedAttributes::Notifications
 
     attr_reader :attributes, :options
 
+    # Public: Creates an instance of AttributeMethodResolver. This instance is
+    # unsually saved on the target_object and used for lookup from the generated
+    # methods.
+    #
+    # base_class - a descendant of ApiCachedAttributes::Base. This class gets
+    #              the list of arguments from this base_class.
+    #
+    # options    - a hash of options. (default: {})
+    #              :scope - the scope option represents the context in which
+    #                       this api resource is unique. It has a similar
+    #                       meaning to ActiveRecord's meaning of scope, as
+    #                       opposed the API access meaning. The scope value can
+    #                       be a Symbol, Array, or Hash. It is used to build the
+    #                       scope argument, which is sent into the client and
+    #                       resource blocks on the Base attributes class. This
+    #                       argument to these blocks is always a hash, whose
+    #                       values were methods responses evaluated on the
+    #                       target_class.
+    #
+    # Returns an instance of AttributeMethodResolver.
     def initialize(base_class, options = {})
       @base_class = base_class
       @attributes = create_attributes!
       @options = ensure_options(options)
     end
 
+    # Public: Lookup the value of the provided method (attribute) name in the
+    # context of the provided target_object.
+    #
+    # method        - A symbol representing the name of the attribute that is to
+    #                 be looked up. This should the same as an attribute defined
+    #                 on the base_class given to the constructor of this class.
+    # target_object - The context object. The scope argument given in the
+    #                 constructor is evaluated on this object. It is usually an
+    #                 instance of a domain object or ActiveRecord model.
+    #
+    # Returns a string of the API's response for this attribute.
     def get(method, target_object)
       attribute = get_copied_attribute_with_target_object(method, target_object)
 
@@ -28,12 +66,17 @@ module ApiCachedAttributes
 
     private
 
+    # Internal: Create an attribute specification for each attribute defined on
+    # the base class. These attributes are copied (duped) and then given their
+    # scope and target_object before being used for lookup.
     def create_attributes!
       @base_class.attributes.map do |method, _value|
         AttributeSpecification.new(method, @base_class)
       end
     end
 
+    # Internal: Returns the already created AttributeSpecification with the
+    # provided name.
     def find_attribute(name)
       @attributes.detect { |attr| attr.name == name }
     end
@@ -47,6 +90,8 @@ module ApiCachedAttributes
       attr
     end
 
+    # Internal: Returns a hash where the values of the scope have been evaluated
+    # on the provided target_object.
     def eval_attribute_scope(target_object)
       scope = {}
       @options[:scope].each_pair do |attr_key, target_method|
@@ -55,6 +100,9 @@ module ApiCachedAttributes
       scope
     end
 
+    # Internal: Normalizes the scope argument. Always returns the scope as a
+    # Hash, despite it being able to be specified as a Symbol, Array, or Hash.
+    # An undefined scope returns an empty Hash.
     def ensure_options(options)
       if ! options[:scope]
         options[:scope] = {}
