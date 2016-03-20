d51f6ed504f28d460b389e86fa287575eb9cc5f0
Chris Ewald <chrisewald@gmail.com>
2016-03-20 01:53:28 -0400

    add instantiation and context to Base class

--- lib/api_cached_attributes/base.rb
+++ lib/api_cached_attributes/base.rb
@@ -7,5 +7,25 @@ module ApiCachedAttributes
 
     extend ApiCachedAttributes::BaseClassMethods
     extend ApiCachedAttributes::Dsl
+
+    def initialize(**args)
+      @scope = args
+      aa = AttributeMethodAttacher.new(self.class, scope: @scope)
+      aa.attach_to(self.class)
+    end
+
+    def client
+      fail ScopeNotSet.new(self.class.name) if @scope == false
+      self.class.client_proc.call(@scope)
+    end
+
+    def resource(name)
+      if (resource = self.class.resources[name])
+        resource.call(client, @scope)
+      else
+        msg = "there is no resource named `#{name}` on #{self.class.name}."
+        fail ArgumentError, msg
+      end
+    end
   end
 end
