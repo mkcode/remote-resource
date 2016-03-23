b9376521e2af4104b7d5da741661005e374bacdf
Chris Ewald <chrisewald@gmail.com>
2016-03-23 13:29:49 -0400

    extract scope evaluation related from resolver

--- lib/api_cached_attributes/attribute_method_resolver.rb
+++ lib/api_cached_attributes/attribute_method_resolver.rb
@@ -93,39 +93,5 @@ module ApiCachedAttributes
       end
       attr
     end
-
-    # Internal: Returns a Boolean indicating whether or not the scope should be
-    # looked up (evaluated on) the target_object. This allows direct values for
-    # the scope to be used, rather then references to methods on the target
-    # object.
-    def should_eval_attribute_scope?
-      @options[:scope].values.all? { |scope_value| scope_value.is_a? Symbol }
-    end
-
-    # Internal: Returns a hash where the values of the scope have been evaluated
-    # on the provided target_object.
-    def eval_attribute_scope(target_object)
-      scope = {}
-      @options[:scope].each_pair do |attr_key, target_method|
-        scope[attr_key.to_sym] = target_object.send(target_method.to_sym)
-      end
-      scope
-    end
-
-    # Internal: Normalizes the scope argument. Always returns the scope as a
-    # Hash, despite it being able to be specified as a Symbol, Array, or Hash.
-    # An undefined scope returns an empty Hash.
-    def ensure_options(options)
-      if ! options[:scope]
-        options[:scope] = {}
-      elsif options[:scope].is_a? Symbol
-        options[:scope] = { options[:scope] => options[:scope] }
-      elsif options[:scope].is_a? Array
-        options[:scope] = {}.tap do |hash|
-          options[:scope].each { |method| hash[method.to_sym] = method.to_sym }
-        end
-      end
-      options
-    end
   end
 end
