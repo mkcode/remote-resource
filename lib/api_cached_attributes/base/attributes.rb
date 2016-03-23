4c186e7a11d62b61a1530af581edc51cb15e10fd
Chris Ewald <chrisewald@gmail.com>
2016-03-23 17:02:55 -0400

    document public methods in Attributes

--- lib/api_cached_attributes/base/attributes.rb
+++ lib/api_cached_attributes/base/attributes.rb
@@ -3,12 +3,26 @@ module ApiCachedAttributes
   module Attributes
     include ApiCachedAttributes::Notifications
 
+    # Public: Creates and returns an array of cached attributes for the provided
+    # base_instance. The base_instance is stored in each attribute and is used
+    # for lookup.
+    #
+    # base_instance - an instance of base_class. The instance means that the
+    # scope has already been applied to it.
+    #
+    # Returns an array a AttributeSpecifications.
     def create_attributes(base_instance)
       @attributes = base_instance.class.attributes.map do |method, _value|
         AttributeSpecification.new(method, base_instance)
       end
     end
 
+    # Public: Returns the value of the provided attribute. It uses the
+    # application configured lookup_method to do so.
+    #
+    # name - a Symbol representing an attribute name
+    #
+    # Returns the value of the attribute as a String.
     def get_attribute(name)
       attribute = find_attribute(name)
 
@@ -18,6 +32,8 @@ module ApiCachedAttributes
         attr_lookup.find(attribute).value
       end
     end
+
+    private
 
     # Internal: Returns the already created AttributeSpecification with the
     # provided name.
