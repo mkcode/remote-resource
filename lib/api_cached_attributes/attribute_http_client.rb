d18187ca516a37c2790359147e1beed7a97191eb
Chris Ewald <chrisewald@gmail.com>
2016-03-24 05:06:49 -0400

    no need for head requests

--- lib/api_cached_attributes/attribute_http_client.rb
+++ lib/api_cached_attributes/attribute_http_client.rb
@@ -10,15 +10,6 @@ module ApiCachedAttributes
       @resource_name = @attribute.resource_name
     end
 
-    def headers_only(additional_headers = {})
-      instrument_attribute('http_head', @attribute) do
-        with_head_only_request(additional_headers) do |client|
-          @attribute.resource(client)
-        end
-      end
-      @client.last_response.headers
-    end
-
     def get(headers = {})
       instrument_attribute('http_get', @attribute) do
         if headers && headers.size > 0
@@ -33,17 +24,6 @@ module ApiCachedAttributes
     end
 
     private
-
-    # Internal: yield a client with the get method swapped for head.
-    def with_head_only_request(headers = {})
-      with_headers_for_method(:head, headers) do |client|
-        client_class = client.singleton_class
-        client_class.send(:alias_method, :orig_get, :get)
-        client_class.send(:alias_method, :get, :head)
-        yield client
-        client_class.send(:alias_method, :get, :orig_get)
-      end
-    end
 
     # Internal: yield a client with headers bound on the supplied method.
     def with_headers_for_method(method, headers)
