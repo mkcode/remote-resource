ec900f299766e5d15aa14c2e65634621b1cd8244
Chris Ewald <chrisewald@gmail.com>
2016-04-29 21:26:00 -0400

    set a ttl on redis storage
    to work with 'volitile-lru' config for redis

--- lib/remote_resource/storage/redis.rb
+++ lib/remote_resource/storage/redis.rb
@@ -1,12 +1,18 @@
+require 'active_support/core_ext/hash/reverse_merge'
+
 require 'remote_resource/storage/serializers/marshal'
 require 'remote_resource/storage/storage_entry'
 
 module RemoteResource
   module Storage
     class Redis
-      def initialize(redis, serializer = nil)
+      def initialize(redis, options = {})
         @redis = redis
-        @serializer = serializer || Serializers::MarshalSerializer.new
+        @options = options.reverse_merge(
+          serializer: Serializers::MarshalSerializer.new,
+          expires_in: 1 * (60 * 60 * 24)
+        )
+        @serializer = @options[:serializer]
       end
 
       def read_key(key)
@@ -20,7 +26,10 @@ module RemoteResource
         storage_entry.to_hash.each_pair do |key, value|
           write_args.concat([key, @serializer.dump(value)]) unless value.empty?
         end
-        @redis.hmset storage_key, *write_args
+        @redis.multi do |multi|
+          multi.hmset storage_key, *write_args
+          multi.expire storage_key, @options[:expires_in]
+        end
       end
     end
   end
